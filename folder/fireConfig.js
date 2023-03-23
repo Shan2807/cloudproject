3cad65d8da9aeb663db4f92a82adee073ffef212
akhils8493 <akhils8493@gmail.com>
2023-03-24 03:42:47 +0530

    cloud

---
+++ folder/fireConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnrfRFnFixf2BfJhOpUaW1hyDjhYDy880",
+  authDomain: "adbms-exp5-6f220.firebaseapp.com",
+  databaseURL: "https://adbms-exp5-6f220-default-rtdb.firebaseio.com",
+  projectId: "adbms-exp5-6f220",
+  storageBucket: "adbms-exp5-6f220.appspot.com",
+  messagingSenderId: "135773193471",
+  appId: "1:135773193471:web:d9570270726bdab0c9b8ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
